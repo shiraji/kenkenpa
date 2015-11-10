3db3d8b6604c62f4860a0d74d9edf05eb38f4632
shiraji <isogai.shiraji@gmail.com>
2015-11-10 14:56:06 +0900

    Added a new test for isLandFirst option

--- kenkenpa-compiler/src/test/java/com/github/shiraji/kenkenpa/compiler/KenKenPaTest.java
+++ kenkenpa-compiler/src/test/java/com/github/shiraji/kenkenpa/compiler/KenKenPaTest.java
@@ -90,5 +90,69 @@ public class KenKenPaTest extends TestCase {
         compareSourceCodesWithoutError(source, expectedSource);
     }
 
+    @Test
+    public void landWithDefaultStateTest() {
+        // @formatter:off
+        JavaFileObject source = JavaFileObjects.forSourceString("test.SimpleFSM", Joiner.on('\n').join(
+            "package test;",
+            "import com.github.shiraji.kenkenpa.annotations.Hop;",
+            "import com.github.shiraji.kenkenpa.annotations.KenKenPa;",
+            "import com.github.shiraji.kenkenpa.annotations.Land;",
+            "@KenKenPa(defaultState = \"CIRCLE1\", isLandFirst = false)",
+            "public abstract class SimpleFSM {",
+            "    @Hop(from = \"CIRCLE2\", to = \"CIRCLE1\")",
+            "    public void fire() {",
+            "      System.out.println(\"fire!\");",
+            "    }",
+            "    @Land(\"CIRCLE1\")",
+            "    public void land() {",
+            "    }",
+            "}"
+        ));
+
+        JavaFileObject expectedSource = JavaFileObjects.forSourceString("test.KenKenPa_SimpleFSM", Joiner.on('\n').join(
+            "package test;",
+            "import com.github.shiraji.kenkenpa.annotations.Hop;",
+            "import java.lang.Override;",
+            "import java.lang.String;",
+            "public final class KenKenPa_SimpleFSM extends SimpleFSM {",
+            "  private String $$mCurrentState$$;",
+            "  KenKenPa_SimpleFSM() {",
+            "    super();",
+            "    this.$$mCurrentState$$ = \"CIRCLE1\";",
+            "  }",
+            "  @Override",
+            "  @Hop(",
+            "      from = \"CIRCLE2\",",
+            "      to = \"CIRCLE1\"",
+            "  )",
+            "  public final void fire() {",
+            "    String newState = takeOff$$fire();",
+            "    super.fire();",
+            "    land$$fire(newState);",
+            "    $$mCurrentState$$ = newState;",
+            "  }",
+            "  private final String takeOff$$fire() {",
+            "    switch($$mCurrentState$$) {",
+            "      case \"CIRCLE2\":",
+            "      return \"CIRCLE1\";",
+            "    }",
+            "    // No definition! Return the default state",
+            "    return \"CIRCLE1\";",
+            "  }",
+            "  private final void land$$fire(String newState) {",
+            "    switch(newState) {",
+            "      case \"CIRCLE1\":",
+            "      land();",
+            "      break;",
+            "    }",
+            "  }",
+            "}"
+        ));
+        // @formatter:on
+
+        compareSourceCodesWithoutError(source, expectedSource);
+    }
+
 
 }
